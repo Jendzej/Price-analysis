b3cd74bb4d3784d66e91a01a107c2f53138356f0
Jędrzej Runowicz <jedrzej.runowicz@gmail.com>
2022-10-22 22:37:19 +0200

    price scrapper

--- app/main.py
+++ app/main.py
@@ -1,5 +1,17 @@
+import requests
+import dotenv
+import os
+from bs4 import BeautifulSoup
+
+
 def main():
-    print("test")
+    dotenv.load_dotenv()
+    url = os.getenv("URL")
+    item = os.getenv("ITEM")
+    response = requests.get(f'{url}q={item.replace(" ", "+")}')
+    prettyResponse = BeautifulSoup(response.content, 'html.parser')
+    price = prettyResponse.find_all("div", {"class":"price-new"})
+    print(f"Most accurate price for your ask ({item}) is: {price[0].text.strip()}")
 
 if __name__ == "__main__":
     main()
