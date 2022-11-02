b6d64089811f158bad24862c694c3b87dc2763bf
Wafelowski <heavywolfpl@gmail.com>
2022-11-02 01:20:04 +0100

    smol improvements

--- app/main.py
+++ app/main.py
@@ -9,11 +9,13 @@ import re
 
 def clearPrice(priceElement: array):
     prices = []
-    for i in range(len(priceElement)):
-        priceElement[i] = priceElement[i].text.strip()
-        priceClear = re.findall("[0-9]|,", priceElement[i].strip())
+    for i, price in enumerate(priceElement): # range(len(priceElement)) is bad, enumerate(priceElement) is better as you get both the index and item
+    # but even if you are not using the index, you can just do "for price in priceElement:"
+        price = price.text.strip()
+        priceClear = re.findall("[0-9]|,", price.strip())
         prices.append(float("".join(priceClear).replace(",",".")))
-    return [priceElement, prices]
+        print("".join(priceClear).replace(",","."))
+    return [priceElement, prices] # That kinda makes sense but why though? What's the point of keeping the dirty version of the price?
 
 
 def avaragePrice(priceList: array):
@@ -23,9 +25,23 @@ def avaragePrice(priceList: array):
 def main():
     dotenv.load_dotenv()
     url = os.getenv("URL")
-    # item = os.getenv("ITEM")
+
+    search = re.search(r"morele\.net\/wyszukiwarka\/", url) # That's one of the ways to make it idiotproof
+    # Since no info is provided on the specs of the URL, we have to assume that user may include
+    # - www.
+    # - https://
+    # - anything after the last slash
+    # We can always add ^ and $ to the regex to make it more strict, and then notify the user on how the URL should look (but they are idiots though?)
+    print(search)
+    if search == None:
+        print("Wrong URL")
+        return
+
     item = input("Type item to search (morele.net): ")
-    response = requests.get(f'{url}q={item.replace(" ", "+")}')
+    response = requests.get(f'{url}?q={item.replace(" ", "+")}')
+    if response.status_code != 200:
+        print("Something went wrong while getting the price from the website.")
+        return
     prettyResponse = BeautifulSoup(response.content, 'html.parser')
     price = prettyResponse.find_all("div", {"class":"price-new"})
     print(f"Most accurate price for your ask ({item}) is: {price[0].text.strip()}")
